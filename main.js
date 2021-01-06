15e28ee7e78c4e4ce4e50859f7a3571c535ab538
Dima <fadeev2990@gmail.com>
2021-01-06 19:12:58 +0400

    countdown timer finished

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const newYears = "1 Jan 2021";
+const daysEl = document.querySelector(".days"),
+      hoursEl = document.querySelector(".hours"),
+      minutesEl = document.querySelector(".minutes"),
+      secondsEl = document.querySelector(".seconds");
+
+const newYears = "1 Jan 2022";
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
-    console.log(newYearsDate-currentDate);
+    const newYearsDate = new Date(newYears),
+          currentDate = new Date(),
+          totalSeconds = (newYearsDate - currentDate) / 1000,
+          days = Math.floor(totalSeconds / 60 / 60 / 24),
+          hours = Math.floor(totalSeconds / 60 / 60) % 24,
+          minutes = Math.floor(totalSeconds / 60) % 60,
+          seconds = Math.floor(totalSeconds % 60);
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
 }
-countdown();
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(countdown, 1000);
+
